632aba50597d37386c4a41d57c05276cf8f78e97
Emir <emiratesbp@gmail.com>
2015-03-23 15:34:34 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="6321 Daniels Parkway, Suite 200"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Fort Myers, FL 33912";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=18556747400;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="pop"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
